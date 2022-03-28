9c9f85d775c5b126627cb8e8774d776c3bd86fc9
Nicolette Staley <emmyandrhys@gmail.com>
2022-03-27 22:59:00 -0400

    started librtar project

---
+++ OdinJavascriptExercises/objects.js
@@ -0,0 +1,9 @@
+function bookMaker (title, author, pages, read){
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = function () {
+		return `${title} by ${author}, ${pages} pages, ${read}`
+		}
+}
