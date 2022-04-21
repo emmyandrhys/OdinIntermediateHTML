d8d39876162c769c52936107e706354548b68de7
Nicolette Staley <emmyandrhys@gmail.com>
2022-04-20 20:29:29 -0400

    workin on createElement for JS

--- LibraryJS/libraryApp.js
+++ LibraryJS/libraryApp.js
@@ -1,27 +1,68 @@
-let myLibrary = [{title: 'My Side of the Mountain', author: 'Jean Craighead George', pages: 177, read: true, info: "'My Side of the Mountain' by Jean Craighead George, 177 pages, read."},
-{title: 'Outdoor School Animal Watching', author: 'Mary Kay Carson', pages: 442, read: false, info:"'Outdoor School Animal Watching' by Mary Kay Carson, 442 pages, not read."},
-{title: 'Mary Poppins Comes Back', author: 'P.L. Travers', pages: 312, read: true, info: "'Mary Poppins Comes Back' by P.L. Travers, 312 pages, read."},
-{title: 'Mary Poppins Opens the Door', author: 'P.L. Travers', pages: 269, read: true, info: "'Mary Poppins Opens the Door' by P.L. Travers, 269 pages, not read."},
-{title: 'Chick Days', author: 'Jenna Woginrich', pages: 128, read: true, info: "'Chick Days' by Jenna Woginrich, 128 pages, read."},
-{title: '', author: '', pages: , read: true, info: ''},
-{title: '', author: '', pages: , read: true, info: ''},
-{title: '', author: '', pages: , read: true, info: ''},
-{title: '', author: '', pages: , read: true, info: ''},
+var myLibrary = [{ title: 'My Side of the Mountain', author: 'Jean Craighead George', pages: 177, read: true, idNum: 0 },
+{ title: 'Outdoor School Animal Watching', author: 'Mary Kay Carson', pages: 442, read: false, idNum: 1 },
+{ title: 'Mary Poppins Comes Back', author: 'P.L. Travers', pages: 312, read: true, idNum: 2 },
+{ title: 'Mary Poppins Opens the Door', author: 'P.L. Travers', pages: 269, read: true, idNum: 3 },
+{ title: 'Chick Days', author: 'Jenna Woginrich', pages: 128, read: true, idNum: 4 },
+	//{title: '', author: '', pages: , read: true, info: ''},
+	//{title: '', author: '', pages: , read: true, info: ''},
+	//{title: '', author: '', pages: , read: true, info: ''},
+	//{title: '', author: '', pages: , read: true, info: ''},
 ];
 
-class Book{
-	constructor(title,author,pages,read){
+class Book {
+	constructor(title, author, pages, read) {
 		this.title = title;
 		this.author = author;
 		this.pages = pages;
 		this.read = read;
-		this.info = function () {
-			return `'${title}' by ${author}, ${pages} pages, ${this.read? 'read': 'not read'}.`
-		}
 	}
-	addBookToLibrary(){
-		let newBook = {title: this.title, author:this.author, pages:this.pages, read:this.read,info:this.info}
+	info() {
+		return `'${title}' by ${author}, ${pages} pages, ${this.read ? 'read' : 'not read'}.`
+	}
+	addBookToLibrary() {
+		let newBook = { title: this.title, author: this.author, pages: this.pages, read: this.read, idNum: myLibrary.length }
 		myLibrary.push(newBook);
 		return;
 	}
 };
+
+const displayLibraryShelf = () => {
+	let libraryShelf = document.getElementById('#shelf');
+	myLibrary.forEach((book) => {
+		const bookElement = document.createElement('div');
+		bookElement.id = `book-${book.idNum}`;
+		bookElement.classList.add('book_card');
+		const bookTitle = document.createElement('h2');
+		bookTitle.classList.add('book_title');
+		bookTitle.innerText = `${book.title}`;
+		const bookAuthor = document.createElement('h3');
+		bookAuthor.classList.add('book_author');
+		bookAuthor.innerText = `${book.author}`;
+		const bookPages = document.createElement('h3');
+		bookPages.classList.add('book_pages');
+		bookPages.innerText = `${book.pages} pages`;
+		const bookRead = document.createElement('btn');
+		bookRead.classList.add('book_read');
+		if (book.read){
+			bookRead.innerText = 'been read';
+			bookRead.classList.add = 'read_true';
+		}
+		else {
+			bookRead.innerText = 'not read';
+			bookRead.classList = 'read_false';
+		}
+		bookRead.addEventListener('click',handle book read) )
+
+
+
+		const bookRemove = document.createElement('btn');
+		bookRemove.classList.add('remove_book');
+		bookRemove.innerText = 'Remove Book';
+		bookElement.appendChild(bookTitle);
+		bookElement.appendChild(bookAuthor);
+		bookElement.appendChild(bookPages)
+		bookElement.appendChild(bookRead);
+		bookElement.appendChild(bookRemove);
+		libraryShelf.appendChild(bookElement)
+	})
+}
