a12f90cdce41be55e1f783868b30dfdb26e22e24
Nicolette Staley <emmyandrhys@gmail.com>
2022-04-24 15:18:00 -0400

    fix rebase issue

--- LibraryJS/libraryApp.js
+++ LibraryJS/libraryApp.js
@@ -26,66 +26,96 @@ class Book {
 	}
 };
 
-const displayLibraryShelf = () => {
-	const libraryShelf = document.getElementById('shelf');
-	myLibrary.forEach((book) => {
-		libraryShelf.appendChild(makeBookCard(book))
+//function to create each book card
+function createBookCard(book){
+	const bookElement = document.createElement('div');
+	bookElement.id = `book-${book.idNum}`;
+	bookElement.classList.add('book_card');
+	const bookTitle = document.createElement('h2');
+	bookTitle.classList.add('book_title');
+	bookTitle.innerText = `${book.title}`;
+	const bookAuthor = document.createElement('h3');
+	bookAuthor.classList.add('book_author');
+	bookAuthor.innerText = `${book.author}`;
+	const bookPages = document.createElement('h3');
+	bookPages.classList.add('book_pages');
+	bookPages.innerText = `${book.pages} pages`;
+	
+	const bookRead = document.createElement('btn');
+	bookRead.classList.add('book_read');
+	bookRead.id = `read-${book.idNum}`;
+	if (book.read){
+		bookRead.innerText = 'been read';
+		bookRead.classList.add('read_true');
+	}
+	else {
+		bookRead.innerText = 'not read';
+		bookRead.classList = 'read_false';
+	}
+	bookRead.addEventListener('click',function(){
+		let bookId = this.target.getAttribute('id').substring(5);
+		if(!myLibrary[bookId][read]){
+			this.target.innerText = 'been read';
+			this.target.classList.add('read_true');
+			this.target.classList.remove('read_false');
+			myLibrary[bookId][read] = true;
+		} 
+	});
+
+	const bookRemove = document.createElement('btn');
+	bookRemove.classList.add('remove_book');
+	bookRead.id = `remove-${book.idNum}`;
+	bookRemove.innerText = 'Remove Book';
+	bookRemove.addEventListener('click', function(){
+		let bookId = this.target.getAttribute('id').substring(7);
+		//book removal confirmation modal
+		//if no, break
+		//if yes, remove book
+		this.parentElement.remove();
+		myLibrary.splice(bookId, 1)
 	})
+
+	bookElement.appendChild(bookTitle);
+	bookElement.appendChild(bookAuthor);
+	bookElement.appendChild(bookPages)
+	bookElement.appendChild(bookRead);
+	bookElement.appendChild(bookRemove);
+
+	return bookElement
 }
 
-function makeBookCard(book){
-		const bookElement = document.createElement('div');
-		bookElement.id = `book-${book.idNum}`;
-		bookElement.classList.add('book_card');
-		bookElement.
-		const bookTitle = document.createElement('h2');
-		bookTitle.classList.add('book_title');
-		bookTitle.innerText = `${book.title}`;
-		const bookAuthor = document.createElement('h3');
-		bookAuthor.classList.add('book_author');
-		bookAuthor.innerText = `${book.author}`;
-		const bookPages = document.createElement('h3');
-		bookPages.classList.add('book_pages');
-		bookPages.innerText = `${book.pages} pages`;
+//render book tiles to shelf
+const libraryShelf = document.getElementById('shelf');
+myLibrary.forEach((book) => {
+	libraryShelf.appendChild(createBookCard(book))
+	})
 
-		const bookRead = document.createElement('btn');
-		bookRead.classList.add('book_read');
-		bookRead.id = `read-${book.idNum}`;
-		if (book.read){
-			bookRead.innerText = 'been read';
-			bookRead.classList.add('read_true');
+
+const addBookBtn = document.getElementById('add-book-btn');
+const form  = document.getElementById('add-book');
+const dupBook = document.getElementById('duplicate-book-warning');
+addBookBtn.addEventListener('click',()=>form.style='display:flex')
+
+
+//grab data from form to add books to database
+form.addEventListener('submit', (event) => {
+    // handle the form data
+	let formTitle = form.elements['title'];
+	let formAuthor = form.elements['author'];
+	let formPages = form.elements['pages'];
+	let formRead = form.elements['read'];
+	
+	//check for duplicate books in library
+	for (let book of myLibrary){
+		if (formTitle === book.title && formAuthor === book.author){
+			dupBook.style = 'display: flex'
+			form.style = 'display:none';
 		}
-		else {
-			bookRead.innerText = 'not read';
-			bookRead.classList = 'read_false';
-		}
-		bookRead.addEventListener('click',function(){
-			let bookId = this.target.getAttribute('id').substring(5);
-			if(!myLibrary[bookId][read]){
-				this.target.innerText = 'been read';
-				this.target.classList.add('read_true');
-				this.target.classList.remove('read_false');
-				myLibrary[bookId][read] = true;
-			}
-		});
-
-		const bookRemove = document.createElement('btn');
-		bookRemove.classList.add('remove_book');
-		bookRead.id = `remove-${book.idNum}`;
-		bookRemove.innerText = 'Remove Book';
-		bookRemove.addEventListener('click', function(){
-			let bookId = this.target.getAttribute('id').substring(7);
-			//book removal confirmation modal
-			//if no, break
-			//if yes, remove book
-			this.parentElement.remove();
-			myLibrary.splice(bookId, 1)
-		})
-
-		bookElement.appendChild(bookTitle);
-		bookElement.appendChild(bookAuthor);
-		bookElement.appendChild(bookPages)
-		bookElement.appendChild(bookRead);
-		bookElement.appendChild(bookRemove);
-		libraryShelf.appendChild(bookElement)
 	}
+
+	//stop form data from being added to database
+	event.preventDefault();
+
+	//allow form data to update database
+	form.submit()
+});
