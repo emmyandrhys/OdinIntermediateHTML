786849535c3657e4c3daa4d9d9b99d1555f73ac9
Nicolette Staley <emmyandrhys@gmail.com>
2022-05-01 00:25:13 -0400

    psuedocode for script layout

---
+++ ticTacToe/script.js
@@ -0,0 +1,37 @@
+//factory function to create players
+//include name, icon, plays, Score
+
+//module to create gameboard
+//eventlisteners to each squares
+//display appropriate image for turn
+
+//game tracking function
+//call match function
+//keep track of score
+//determine if the game has a winner
+//determine if the game is a cat's game (tie)
+//call modal at end of match and end of game
+
+//match tracking function
+//keeps track of turn
+//places icon in square as clicked
+//determines if match is over or not.
+//passes winner (or lack of) to game fucntion
+
+//new game modal fucntion
+//display modal
+//add eventlisteners
+//get player names and icons
+//call game function
+
+//end match modal function
+//displays appropriate modal for Score
+//displays score and player name in modal
+//adds eventlisteners to buttons
+//calls new match or end game function
+
+//end game modal fucntion
+//displays appropriate modal for Score
+//displays score and player name in modal
+//adds eventlisteners to buttons
+//calls new game function or closes
