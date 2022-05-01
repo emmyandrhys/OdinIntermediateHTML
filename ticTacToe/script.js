4164c8fa9014e00fd4b39b48c039d1222496b907
Nicolette Staley <emmyandrhys@gmail.com>
2022-05-01 01:00:40 -0400

    player Factory function

--- ticTacToe/script.js
+++ ticTacToe/script.js
@@ -1,5 +1,39 @@
+// const to check if there is a winner
+const winRound = [[1,2,3], [1,5,9], [1,4,7], [2,5,8], [3,6,9], [3,5,7], [4,5,6], [7, 8, 9]]
+
 //factory function to create players
 //include name, icon, plays, Score
+const playerFactory = (name, icon, score=0) => {
+  const getName = () => name;
+  const getIcon = () => icon;
+  const getScore = () => score;
+  let record = []
+
+  const winRound = () => { score += 1 }
+  const checkWin = () => {
+    if ( winRound.includes(record[-1])) {
+      winRound();
+      return true;
+    } else { return false }
+  }
+  const recordMove = play => {
+    record[-1].push(play);
+    if (record[-1].length >= 3 ){
+      record[-1].sort();
+      return checkWin();
+    } else { return false }
+  }
+  const resetPlayer = () => {
+    name = null;
+    icon = null;
+    score = null;
+    record = null;
+  }
+  const recordNewRound = () => {
+    record.push([]);
+  }
+  return { recordMove, recordNewRound, resetPlayer, getName, getScore, getIcon }
+}
 
 //module to create gameboard
 //eventlisteners to each squares
