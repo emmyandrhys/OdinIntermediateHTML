0c13da27ce355a1a2b4e7673e850ae3a784d175a
Nicolette Staley <emmyandrhys@gmail.com>
2022-06-17 15:41:30 -0400

    added CanvasAPIs A

--- ticTacToe/script.js
+++ ticTacToe/script.js
@@ -1,10 +1,10 @@
 // const to check if there is a winner
-const winRoundCheckRecords = [[1,2,3], [1,5,9], [1,4,7], [2,5,8], [3,6,9], [3,5,7], [4,5,6], [7, 8, 9]]
+const winRoundCheckRecords = [[1, 2, 3], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]
 
 //factory function to create players
 //include name, icon, plays, Score
 //return functions: recordMove, resetPlayer, recordNewRound, getName, getScore, getIcon
-const playerFactory = (name, icon, score=0) => {
+const playerFactory = (name, icon, score = 0) => {
   const getName = () => name;
   const getIcon = () => icon;
   const getScore = () => score;
@@ -12,21 +12,21 @@ const playerFactory = (name, icon, score=0) => {
 
   const winRound = () => { score += 1 }
   const checkWin = (check) => {
-      let win = false;
-      while (!win){
-        for(let value of winRoundCheck){
-          let num = 0;
-          if(value.forEach(element => {
-            check.includes(element)? num +=1:false;
-          }));
-          num===3? win=true: false;
-        }
-      } if (win) { winRound()}
-      return win;
+    let win = false;
+    while (!win) {
+      for (let value of winRoundCheck) {
+        let num = 0;
+        if (value.forEach(element => {
+          check.includes(element) ? num += 1 : false;
+        }));
+        num === 3 ? win = true : false;
+      }
+    } if (win) { winRound() }
+    return win;
   }
   const recordMove = play => {
     record[-1].push(play);
-    if (record[-1].length >= 3 ){
+    if (record[-1].length >= 3) {
       record[-1].sort();
       return checkWin(record[-1]);
     } else { return false }
@@ -56,39 +56,66 @@ const match = (e, turn) => {
 
 
   if (first != begun)
-  if (document.querySelectorAll('.hidden').length === 7){
-    let x = false;
-    while (x === false){
-      let square = e.target;
-      let play = square.id;
+    if (document.querySelectorAll('.hidden').length === 7) {
+      let x = false;
+      while (x === false) {
+        let square = e.target;
+        let play = square.id;
+      }
+      //passes winner (or lack of) to game function
+      //game(player)
     }
-//passes winner (or lack of) to game function
-//game(player)
-  }
 }
 
 // grab all necessary elements for the DOM
+//gameboard
 const board = document.getElementById('game-board');
+//scoreboard elements
 const p1nameHeader = document.getElementById('player-1-name');
 const p2nameHeader = document.getElementById('player-2-name');
 const p1scoreHeader = document.getElementById('player-1-score');
 const p2scoreHeader = document.getElementById('player-2-score');
+//get player info/new game elements
 const getPlayersModal = document.getElementById('get-players');
 const form = document.getElementById('player-form');
 const player1Form = document.getElementById('player1-info');
 const player2Form = document.getElementById('player2-info');
-
+const iconArea = document.getElementById('icon-choices');
 const Player1Button = document.getElementById('submit-button')
 const Player2Button = document.getElementById('start-game-button')
+//playAgainModel
 const playAgainModal = document.getElementById('play-again')
+//finalScoreModal
 const finalScoreModal = document.getElementById('final-score-modal');
+//catGameModal
 const catGameModal = document.getElementById('cat-game');
+//tiemodals
 const tiePlayAgainModal = document.getElementById('tie-play-again');
 const catGameTieModal = document.getElementById('cat-game-tie');
 const finalScoreTieModal = document.getElementById('final-score-tie');
 
-
-
+const iconList = [
+  { id: "chicken-head", source: "./assets/chicken-head.svg" },
+  { id: "chicken", source: "./assets/chicken.svg" },
+  { id: "ChickenHead", source: "./assets/ChickenHead.svg" },
+  { id: "ColorfulRooster", source: "./assets/ColorfulRooster.svg" },
+  { id: "Cute_chick", source: "./assets/Cute_chick.svg" },
+  { id: "drawing", source: "./assets/drawing.svg" },
+  { id: "eggscape", source: "./assets/eggscape.svg" },
+  { id: "eggstracute", source: "./assets/eggstracute.svg" },
+  { id: "FancyEggChick", source: "./assets/FancyEggChick.svg" },
+  { id: "FancyFloralEgg", source: "./assets/FancyFloralEgg.svg" },
+  { id: "FancyMandalaEgg", source: "./assets/FancyMandalaEgg.svg" },
+  { id: "HatchedChick", source: "./assets/HatchedChick.svg" },
+  { id: "HatchingChickSide", source: "./assets/HatchingChickSide.svg" },
+  { id: "Hen", source: "./assets/Hen.svg" },
+  { id: "rooster", source: "./assets/rooster.svg" },
+  { id: "ThreeEggs", source: "./assets/ThreeEggs.svg" },
+]
+//icon selection variable
+let selected = null;
+const iconWarning = document.getElementById('icon-warning')
+const iconChoices = document.getElementById('icon-choices')
 
 
 
@@ -109,7 +136,7 @@ const gameBoard = (() => {
   const buildBoard = (board) => {
     let x = 1;
     //create 9 squares in board
-    while (x < 10){
+    while (x < 10) {
       let square = document.createElement('div');
       square.classList.add('game-square');
       square.setAttribute('id', `${x}`);
@@ -133,29 +160,31 @@ const gameBoard = (() => {
     })
   };
 
-  //icon selection variable
-  let selected = null;
-  const iconWarning = document.getElementById('icon-warning')
-
   //new game modal function
   const newGame = () => {
     //display modal
     getPlayersModal.classList.remove('noshow');
-    let icons = iconArea.children;
-      for (let icon of icons){
-        icon.addEventListener('click', (e)=>{
-          selected = e.target.id
-        })
-      }
+    //icon choice modal builder
+    iconList.forEach((n) => {
+      let icon = document.createElement('img');
+      icon.classList.add('icon-img');
+      icon.setAttribute('id', `${n[id]}`);
+      icon.setAttribute('src', n[source])
+      icon.addEventListener('click', (e) => {
+        selected = e.target.getAttribute('src')
+      })
+      iconChoices.appendChild(icon);
+    });
+
     //add eventlistener to Player-1-button
-    Player1Button.addEventListener('click', function player1form () {
+    Player1Button.addEventListener('click', function player1form() {
       let formData = document.getElementById('player-form').elements;
       //get player1 name and icon
       let player1name = formData[0].value;
       //if icon not selected throw warning
-      if(selected === null){
+      if (selected === null) {
         iconWarning.classList.remove('noshow');
-        document.getElementById('icon-warning.btn').addEventListener('click', ()=>{iconWarning.classList.add('noshow')})
+        document.getElementById('icon-warning.btn').addEventListener('click', () => { iconWarning.classList.add('noshow') })
         return;
       } //if icon selected construct player1 and display player 2 form
       else {
@@ -170,56 +199,25 @@ const gameBoard = (() => {
       }
     })
     //add event listener to player2 button
-    Player2Button.addEventListener('click', function player2form(){
+    Player2Button.addEventListener('click', function player2form() {
       let formData = document.getElementById('player-form').elements;
       //get player2 name and icon
       let player2name = formData[1].value;
       //if icon selected matches player1 icon, throw warning
-      if(selected === player1.getIcon) {
+      if (selected === player1.getIcon) {
         iconWarning.classList.remove('noshow');
       } //if icon selected is different from player1, construct player2 and hide modal
       else {
         let player2icon = selected;
         const player2 = playerFactory(player2name, player2icon);
         getPlayersModal.classList.add('noshow');
-        return player2;
+        squareImages(player1, player2)
       }
-    })   
+    })
   }
-  return {buildBoard, newGame, }
+  return { buildBoard, newGame, }
 })();
 gameBoard.buildBoard(board);
-newGame()
+gameBoard.newGame()
 
-const iconList = [
-  {id: "chicken-head", source: "./assets/chicken-head.svg"},
-  {id: "chicken", source: "./assets/chicken.svg"},
-  {id: "ChickenHead", source: "./assets/ChickenHead.svg"},
-  {id: "ColorfulRooster", source: "./assets/ColorfulRooster.svg"},
-  {id: "Cute_chick", source: "./assets/Cute_chick.svg"},
-  {id: "drawing", source: "./assets/drawing.svg"},
-  {id: "eggscape", source: "./assets/eggscape.svg"},
-  {id: "eggstracute", source: "./assets/eggstracute.svg"},
-  {id: "FancyEggChick", source: "./assets/FancyEggChick.svg"},
-  {id: "FancyFloralEgg", source: "./assets/FancyFloralEgg.svg"},
-  {id: "FancyMandalaEgg", source: "./assets/FancyMandalaEgg.svg"},
-  {id: "HatchedChick", source: "./assets/HatchedChick.svg"},
-  {id: "HatchingChickSide", source: "./assets/HatchingChickSide.svg"},
-  {id: "Hen", source: "./assets/Hen.svg"},
-  {id: "rooster", source: "./assets/rooster.svg"},
-  {id: "ThreeEggs", source: "./assets/ThreeEggs.svg"},
-]
-let iconArea = document.getElementById('icon-choices');
 
-//icon choice modal builder
-const iconChoices = () => {
-  iconList.forEach(n => {
-    let icon = document.createElement('img');
-    icon.classList.add('icon-img');
-    icon.setAttribute('id', `${n[id]}`);
-    icon.setAttribute('src', n[source])
-    iconChoices.appendChild(icon);
-  })
-};
-
-//function to determine selected icons
